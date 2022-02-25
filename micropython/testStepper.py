40bf70714339eca9ab6329c355a218edf3691ba9
papsdroid.fr <32851720+papsdroidfr@users.noreply.github.com>
2022-02-25 21:29:04 +0100

    Update testStepper.py

--- micropython/testStepper.py
+++ micropython/testStepper.py
@@ -5,11 +5,11 @@ import utime
 from bipolarStepper import BipolarStepper
 
 bp = BipolarStepper(speed='medium')
-bp.next_step(100) 
+bp.next_steps(100) 
 utime.sleep(0.5)
 bp.set_speed('high')
 bp.set_direction('backward')
-bp.next_step(50)
+bp.next_steps(50)
 
 
 
